ea93cd317e485d3a243e6f8764514d8988591cf4
wmdaros <wmdaros@65fb43be-c858-11de-a2e8-8d36a3108c74>
2011-04-19 10:21:08 +0000

    - corporate-pom version 2.11 - liferay-camel integration

---
+++ trunk/core-bc/modules/liferay-camel/src/main/java/com/liferay/portal/esb/camel/EndpointMessageListener.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright (c) 2000-2009 Liferay, Inc. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.liferay.portal.esb.camel;
+
+import com.liferay.portal.kernel.messaging.Message;
+import com.liferay.portal.kernel.messaging.MessageListener;
+
+import org.apache.camel.Endpoint;
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.apache.camel.impl.DefaultExchange;
+import org.apache.camel.impl.DefaultMessage;
+
+/**
+ * @author Bruno Farache
+ */
+public class EndpointMessageListener implements MessageListener {
+
+	public EndpointMessageListener(Endpoint endpoint, Processor processor) {
+		_endpoint = endpoint;
+		_processor = processor;
+	}
+	
+	public void receive(Message message) {
+		try {
+			_processor.process(createExchange(message));
+		} 
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public Exchange createExchange(Message message) {
+		Exchange exchange = new DefaultExchange(_endpoint);
+		org.apache.camel.Message in = new DefaultMessage();
+		
+		in.setBody(message.getPayload());
+		
+		exchange.setIn(in);
+
+		return exchange;
+	}
+	
+	private Endpoint _endpoint;
+	private Processor _processor;
+	
+}
