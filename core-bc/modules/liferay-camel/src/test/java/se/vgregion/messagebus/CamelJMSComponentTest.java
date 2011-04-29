56ddb0a04b00dec53a0bd45f62c0303cdf95abf4
patrik.bergstrom@softronic.se <patrik.bergstrom@softronic.se@65fb43be-c858-11de-a2e8-8d36a3108c74>
2011-04-29 09:32:54 +0000

    Camel message bus communication, some views and controllers.

--- core-bc/modules/liferay-camel/src/test/java/se/vgregion/messagebus/CamelJMSComponentTest.java
+++ core-bc/modules/liferay-camel/src/test/java/se/vgregion/messagebus/CamelJMSComponentTest.java
@@ -1,20 +1,24 @@
 package se.vgregion.messagebus;
 
-import se.vgregion.messagebus.util.SimpleMessageListener;
 import com.liferay.portal.kernel.messaging.Message;
 import com.liferay.portal.kernel.messaging.MessageBus;
 import com.liferay.portal.kernel.messaging.MessageListener;
+import org.apache.activemq.ActiveMQConnection;
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.jms.core.MessageCreator;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit38.AbstractJUnit38SpringContextTests;
+import se.vgregion.messagebus.util.SimpleMessageListener;
 
-import javax.jms.JMSException;
-import javax.jms.Session;
+import javax.jms.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 /**
  * @author Bruno Farache
@@ -25,13 +29,20 @@ import java.util.List;
                 "/META-INF/test-routes.xml", "/META-INF/jms-messaging.xml"})
 public class CamelJMSComponentTest extends AbstractJUnit38SpringContextTests {
 
+    private static final Logger log = Logger.getLogger(CamelJMSComponentTest.class);
+
+    @Value("${messagebus.destination}")
+    String messagebusDestination;
+    @Value("${activemq.destination}")
+    String activemqDestination;
+
     @DirtiesContext
     public void testJMSMessage() throws Exception {
         SimpleMessageListener.messageReceived = false;
         final String msg = "A test message";
 
         //Send to SimpleMessageListener
-        jmsTemplate.send("TEST.QUEUE", new MessageCreator() {
+        jmsTemplate.send(activemqDestination, new MessageCreator() {
             @Override
             public javax.jms.Message createMessage(Session session) throws JMSException {
                 return session.createTextMessage(msg);
@@ -45,7 +56,7 @@ public class CamelJMSComponentTest extends AbstractJUnit38SpringContextTests {
     }
 
     @DirtiesContext
-    public void testMessageBusToJms() throws Exception{
+    public void testMessageBusToJms() throws Exception {
         SimpleMessageListener.messageReceived = false;
         String payload = "test payload";
 
@@ -70,31 +81,48 @@ public class CamelJMSComponentTest extends AbstractJUnit38SpringContextTests {
     @DirtiesContext
     public void testCamelQueueReply() throws Exception {
         final String payload = "apa bepa";
+        List<String> correlationIds = new ArrayList<String>();
+        correlationIds.add(UUID.randomUUID().toString());
+        correlationIds.add(UUID.randomUUID().toString());
+
+
         Message message = new Message();
         message.setPayload(payload);
+        message.setResponseId(correlationIds.get(0));
+        Message message2 = new Message();
+        message2.setPayload(payload);
+        message2.setResponseId(correlationIds.get(1));
 
-        final List<Object> list = new ArrayList();
+        final List<Message> list = new ArrayList();
 
-        messageBus.registerMessageListener("vgr/testCamelDestinationReply", new MessageListener() {
+        messageBus.registerMessageListener(messagebusDestination + ".REPLY", new MessageListener() {
             @Override
             public void receive(Message message) {
                 assertEquals(payload.toUpperCase(), message.getPayload());
-                list.add(new Object());
+                list.add(message);
             }
         });
 
-        messageBus.sendMessage("vgr/testCamelDestination", message);
+        messageBus.sendMessage(messagebusDestination, message);
+        Thread.sleep(10);
+        messageBus.sendMessage(messagebusDestination, message2);
 
         Thread.sleep(1000);
 
-        assertEquals(1, list.size());
+        assertEquals(2, list.size());
+        for (Message m : list) {
+            assertTrue(correlationIds.contains(m.getResponseId()));
+            correlationIds.remove(m.getResponseId());
+        }
+
+        assertEquals(0, correlationIds.size());
     }
 
 
     private List<Object> sendToMessageBus(final String payload) {
         final List<Object> list = new ArrayList();
 
-        String destinationName = "vgr/testJmsDestination";
+        String destinationName = messagebusDestination;
 
         Message message = new Message();
         message.setPayload(payload);
