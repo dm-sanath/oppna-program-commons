2fb3b7ffac00ea9177417fba29e1e358439be183
wmdaros <wmdaros@65fb43be-c858-11de-a2e8-8d36a3108c74>
2011-05-06 14:14:38 +0000

    - Generic marshal/unmarshal for Jaxb objects - create, invite routes added (configuration) - validation on -> logged in user == sponsor, and that sponsor is vgr - test configuration cleanup

--- trunk/core-bc/modules/liferay-camel/src/test/java/se/vgregion/messagebus/MessageBusComponentTest.java
+++ trunk/core-bc/modules/liferay-camel/src/test/java/se/vgregion/messagebus/MessageBusComponentTest.java
@@ -45,8 +45,8 @@ import org.springframework.test.context.junit38.AbstractJUnit38SpringContextTest
  */
 @ContextConfiguration(
 	locations={
-		"/META-INF/message-bus-spring.xml", "/META-INF/spring/camel-spring.xml",
-		"/META-INF/test-routes.xml"})
+		"/META-INF/message-bus-spring-test.xml", "/META-INF/camel-spring-test.xml",
+		"/META-INF/test-routes.xml", "/META-INF/jms-messaging.xml"})
 public class MessageBusComponentTest extends AbstractJUnit38SpringContextTests {
 	
 	@DirtiesContext
