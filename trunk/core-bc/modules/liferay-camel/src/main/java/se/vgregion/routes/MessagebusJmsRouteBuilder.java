423ff88bed55daa93d6bcbefa6b31d8b86b327a0
patrik.bergstrom@softronic.se <patrik.bergstrom@softronic.se@65fb43be-c858-11de-a2e8-8d36a3108c74>
2011-05-23 13:48:45 +0000

    exception handling on connection failed etc

--- trunk/core-bc/modules/liferay-camel/src/main/java/se/vgregion/routes/MessagebusJmsRouteBuilder.java
+++ trunk/core-bc/modules/liferay-camel/src/main/java/se/vgregion/routes/MessagebusJmsRouteBuilder.java
@@ -1,12 +1,11 @@
 package se.vgregion.routes;
 
+import com.liferay.portal.kernel.messaging.DestinationNames;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.apache.camel.spring.SpringRouteBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 
 /**
  * User: pabe
@@ -36,11 +35,37 @@ public class MessagebusJmsRouteBuilder extends SpringRouteBuilder {
     @Override
     public void configure() throws Exception {
         from("liferay:" + messageBusDestination)
+                .errorHandler(deadLetterChannel("direct:error_" + messageBusDestination))
                 .setHeader("JMSCorrelationID", header("responseId"))
                 .to("activemq:queue:" + activeMqDestination + "?preserveMessageQos=true&replyTo=" + activeMqDestination + ".REPLY");
 
         from("activemq:queue:" + activeMqDestination + ".REPLY?disableReplyTo=true")
                 .setHeader("responseId", header("JMSCorrelationID"))
-                .to("liferay:" + messageBusDestination + ".REPLY");
+                .to("liferay:" + DestinationNames.MESSAGE_BUS_DEFAULT_RESPONSE);
+
+        from("direct:error_" + messageBusDestination)
+                .process(new Processor() {
+                    @Override
+                    public void process(Exchange exchange) throws Exception {
+                        //Handle connection error
+                        Object exception = exchange.getProperty(Exchange.EXCEPTION_CAUGHT);
+                        if (exception instanceof Throwable) {
+                            Throwable ex = (Throwable) exception;
+                            while (ex.getCause() != null) {
+                                ex = ex.getCause();
+                            }
+                            if (ex.getClass().getPackage().getName().startsWith("java.net")) {
+                                exchange.getOut().setBody(ex);
+                            } else {
+                                exchange.getOut().setBody(new Exception(((Throwable) exception).getMessage()));
+                            }
+                        } else {
+                            exchange.getOut().setBody(new Exception("Unknown error"));
+                        }
+                        String responseId = (String) exchange.getIn().getHeader("JMSCorrelationID");
+                        exchange.getOut().setHeader("responseId", responseId);
+                    }
+                })
+                .to("liferay:" + DestinationNames.MESSAGE_BUS_DEFAULT_RESPONSE);
     }
 }
