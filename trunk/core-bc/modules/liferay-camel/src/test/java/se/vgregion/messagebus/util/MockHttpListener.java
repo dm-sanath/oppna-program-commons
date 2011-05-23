61088edc9527b8fd491b4382d77c5e03b949b529
patrik.bergstrom@softronic.se <patrik.bergstrom@softronic.se@65fb43be-c858-11de-a2e8-8d36a3108c74>
2011-05-23 15:30:25 +0000

    - MockHttpListener - InvitePreferencesPropertyEditor can set null - JaxbUtilTest

---
+++ trunk/core-bc/modules/liferay-camel/src/test/java/se/vgregion/messagebus/util/MockHttpListener.java
@@ -0,0 +1,40 @@
+package se.vgregion.messagebus.util;
+
+import org.mortbay.jetty.Server;
+import org.mortbay.jetty.handler.AbstractHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Random;
+
+/**
+ * User: pabe
+ * Date: 2011-05-23
+ * Time: 16:59
+ */
+public class MockHttpListener {
+
+    private static Server server = new Server(8899);
+
+    public static void main(String[] args) throws Exception {
+        server.addHandler(new AbstractHandler() {
+            @Override
+            public void handle(String s, HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, int i) throws IOException, ServletException {
+                System.out.println(httpServletRequest);
+                PrintWriter writer = httpServletResponse.getWriter();
+                writer.append("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>" +
+                "<createUserResponse xmlns=\"http://portal.vgregion.se/createuser\">" +
+                "<vgrId>ex_test</vgrId>" +
+                "<statusCode>NEW_USER</statusCode>" +
+                "<message>message</message>" +
+                "</createUserResponse>");
+                writer.close();
+                httpServletResponse.setStatus(HttpServletResponse.SC_OK);
+            }
+        });
+        server.start();
+    }
+}
