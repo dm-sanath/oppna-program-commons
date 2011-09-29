9f454ea2fce9100f68f3302e1a3fb4d5e3d6dfbe
wmdaros <wmdaros@65fb43be-c858-11de-a2e8-8d36a3108c74>
2011-09-29 09:48:54 +0000

    [maven-release-plugin]  copy for tag commons-util-system-3.7

---
+++ tags/commons-util-system-3.7/core-bc/composites/svc-camel/src/main/java/se/vgregion/messagebus/EndpointMessageListener.java
@@ -0,0 +1,93 @@
+/**
+ * Copyright (c) 2000-2009 Liferay, Inc. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package se.vgregion.messagebus;
+
+import com.liferay.portal.kernel.messaging.Message;
+import com.liferay.portal.kernel.messaging.MessageListener;
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.apache.camel.impl.DefaultExchange;
+import org.apache.camel.impl.DefaultMessage;
+
+import java.util.Map;
+
+/**
+ * Endpoint message listener.
+ * <p/>
+ * @author Bruno Farache
+ */
+public class EndpointMessageListener implements MessageListener {
+
+    /**
+     * Constructor.
+     *
+     * @param endpoint  endpoint.
+     * @param processor processor.
+     */
+    public EndpointMessageListener(MessageBusEndpoint endpoint, Processor processor) {
+        this.endpoint = endpoint;
+        this.processor = processor;
+    }
+
+    /**
+     * Outbound message.
+     *
+     * @param message message to send.
+     */
+    public void receive(Message message) {
+        try {
+            processor.process(createExchange(message));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private Exchange createExchange(Message message) {
+        Exchange exchange = new DefaultExchange(endpoint);
+        org.apache.camel.Message in = new DefaultMessage();
+
+        in.setBody(message.getPayload());
+        in.setHeader("responseId", message.getResponseId());
+
+        Map<String, Object> params = endpoint.getParams();
+        if (params != null) {
+            String inHeaderKeys = (String) params.get("MessageInHeaders");
+            if (inHeaderKeys != null) {
+                for (String key : inHeaderKeys.split(",")) {
+                    Object value = message.get(key);
+                    if (value != null) {
+                        in.setHeader(key, value);
+                    }
+                }
+            }
+        }
+
+        exchange.setIn(in);
+
+        return exchange;
+    }
+
+    private MessageBusEndpoint endpoint;
+    private Processor processor;
+
+}
