3c67b997ac1386b6081e613ea5a5ae393c2d273f
patrik.bergstrom@softronic.se <patrik.bergstrom@softronic.se@65fb43be-c858-11de-a2e8-8d36a3108c74>
2011-10-28 08:27:37 +0000

    - Added HttpRequest which is handled by EndpointMessageListener - Added tests with HttpRequest

---
+++ core-bc/composites/svc-camel/src/main/java/se/vgregion/http/HttpRequest.java
@@ -0,0 +1,60 @@
+package se.vgregion.http;
+
+
+import java.util.Map;
+
+/**
+ * @author Patrik Bergström
+ */
+public class HttpRequest {
+
+    private HttpMethod method;
+    private String queryString;
+    private String body;
+
+    public void setQueryByString(String queryString) {
+        this.queryString = queryString;
+    }
+
+    public void setQueryByMap(Map<String, String> queryMap) {
+        StringBuilder sb = mapToQuery(queryMap);
+        this.queryString = sb.toString();
+    }
+
+    public String getQueryString() {
+        return queryString;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public HttpMethod getMethod() {
+        return method;
+    }
+
+    public void setMethod(HttpMethod method) {
+        this.method = method;
+    }
+
+    public static StringBuilder mapToQuery(Map<String, String> parameters) {
+        StringBuilder sb = new StringBuilder();
+        int count = 0;
+        for (Map.Entry<String, String> key : parameters.entrySet()) {
+            sb.append(key.getKey() + "=" + key.getValue());
+            count++;
+            if (count < parameters.size()) {
+                sb.append("&"); //we will have at least one more parameter to add to our query
+            }
+        }
+        return sb;
+    }
+
+    public enum HttpMethod {
+        POST, GET;
+    }
+}
