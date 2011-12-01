378a7150434c15e1ce9f78657b986042f535e2bc
wmdaros <wmdaros@65fb43be-c858-11de-a2e8-8d36a3108c74>
2011-12-01 09:26:31 +0000

    - OrganizationHelper interface - Work in progress...

--- trunk/core-bc/composites/svc-liferay/src/main/java/se/vgregion/liferay/organization/OrganizationHelper.java
+++ trunk/core-bc/composites/svc-liferay/src/main/java/se/vgregion/liferay/organization/OrganizationHelper.java
@@ -3,6 +3,8 @@ package se.vgregion.liferay.organization;
 import com.liferay.portal.model.Organization;
 import com.liferay.portal.model.User;
 
+import java.util.List;
+
 /**
  * Created by IntelliJ IDEA.
  * Created: 2011-11-29 15:32
@@ -10,15 +12,14 @@ import com.liferay.portal.model.User;
  * @author <a href="mailto:david.rosell@redpill-linpro.com">David Rosell</a>
  */
 public interface OrganizationHelper {
-    void addUser(Organization organization, User... user);
-    void addUser(String organizationName, User... user);
+    void addUser(List<String> organizationNames, User user);
 
-    void removeUser(Organization organization, User... user);
-    void removeUser(String organizationName, User... user);
+    void removeUser(List<String> organizationNames, User user);
 
     Organization findByName(String organizationName, long companyId);
 
     boolean isMember(Organization organization, User user);
+    boolean isMember(String organizationName, User user);
 
     void createIfNeeded(String organizationName, long companyId);
 
--- trunk/core-bc/composites/svc-liferay/src/main/java/se/vgregion/liferay/organization/OrganizationHelperImpl.java
+++ trunk/core-bc/composites/svc-liferay/src/main/java/se/vgregion/liferay/organization/OrganizationHelperImpl.java
@@ -1,9 +1,16 @@
 package se.vgregion.liferay.organization;
 
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.model.Organization;
 import com.liferay.portal.model.User;
 import com.liferay.portal.service.OrganizationLocalService;
+import com.liferay.portal.service.UserLocalService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -12,28 +19,52 @@ import org.springframework.beans.factory.annotation.Autowired;
  * @author <a href="mailto:david.rosell@redpill-linpro.com">David Rosell</a>
  */
 public class OrganizationHelperImpl implements OrganizationHelper {
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrganizationHelperImpl.class);
 
     @Autowired
     private OrganizationLocalService organizationLocalService;
 
+    @Autowired
+    private UserLocalService userLocalService;
+
     @Override
-    public void addUser(Organization organization, User... user) {
-        //To change body of implemented methods use File | Settings | File Templates.
+    public void addUser(List<String> organizationNames, User user) {
+
+        for (String organizationName: organizationNames) {
+            try {
+                long organizationId = organizationLocalService.getOrganizationId(user.getCompanyId(),
+                        organizationName);
+
+                userLocalService.addOrganizationUsers(organizationId, new long[]{user.getUserId()});
+
+            } catch (Exception e) {
+                String msg = String.format("Failed to add user [%s] to organization [%s]",
+                        user.getFullName(), organizationName);
+                log(msg, e);
+            }
+        }
     }
 
     @Override
-    public void addUser(String organizationName, User... user) {
-        //To change body of implemented methods use File | Settings | File Templates.
-    }
+    public void removeUser(List<String> organizationNames, User user) {
+        long[] users= {user.getUserId()};
+        for (String organizationName: organizationNames) {
+            try {
+                long organizationId = organizationLocalService.getOrganizationId(user.getCompanyId(),
+                        organizationName);
 
-    @Override
-    public void removeUser(Organization organization, User... user) {
-        //To change body of implemented methods use File | Settings | File Templates.
-    }
 
-    @Override
-    public void removeUser(String organizationName, User... user) {
-        //To change body of implemented methods use File | Settings | File Templates.
+                userLocalService.unsetOrganizationUsers(organizationId, new long[]{user.getUserId()});
+
+            } catch (Exception e) {
+                String msg = String.format("Failed to add user [%s] to organization [%s]",
+                        user.getFullName(), organizationName);
+                log(msg, e);
+            }
+        }
+
+
+
     }
 
     @Override
@@ -47,7 +78,34 @@ public class OrganizationHelperImpl implements OrganizationHelper {
     }
 
     @Override
+    public boolean isMember(String organizationName, User user) {
+        try {
+            List<Organization> userOrganizations = user.getOrganizations();
+            for (Organization organization : userOrganizations) {
+                if (organization.getName().equals(organizationName)) {
+                    return true;
+                }
+            }
+            return false;
+        } catch (Exception e) {
+            String msg = String.format("Failed to lookup if user [%s] is member in organization [%s]",
+                    user.getScreenName(), organizationName);
+            log(msg, e);
+            throw new RuntimeException(msg, e);
+        }
+    }
+
+    @Override
     public void createIfNeeded(String organizationName, long companyId) {
         //To change body of implemented methods use File | Settings | File Templates.
     }
+
+    private void log(String msg, Exception e) {
+        if (LOGGER.isDebugEnabled()) {
+            LOGGER.warn(msg, e);
+        } else {
+            LOGGER.warn(msg);
+        }
+    }
+
 }
