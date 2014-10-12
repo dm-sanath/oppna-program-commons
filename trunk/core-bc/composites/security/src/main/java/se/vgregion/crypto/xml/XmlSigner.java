e3759cefb5d109c986a857b8a3d843289e6da123
patrik.bergstrom@outlook.com <patrik.bergstrom@outlook.com@65fb43be-c858-11de-a2e8-8d36a3108c74>
2014-10-12 18:40:48 +0000

    Added xml dsig validator.

--- trunk/core-bc/composites/security/src/main/java/se/vgregion/crypto/xml/XmlSigner.java
+++ trunk/core-bc/composites/security/src/main/java/se/vgregion/crypto/xml/XmlSigner.java
@@ -1,9 +1,11 @@
 package se.vgregion.crypto.xml;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 import se.vgregion.certificate.PkixUtil;
 
+import javax.xml.crypto.dom.DOMStructure;
 import javax.xml.crypto.dsig.*;
 import javax.xml.crypto.dsig.dom.DOMSignContext;
 import javax.xml.crypto.dsig.dom.DOMValidateContext;
@@ -19,8 +21,7 @@ import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.security.KeyStore;
-import java.security.PublicKey;
+import java.security.*;
 import java.security.cert.X509Certificate;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -28,7 +29,66 @@ import java.util.List;
 
 public class XmlSigner {
 
+    public String signEnveloping(String xml, KeyStore.PrivateKeyEntry privateKeyEntry) {
+
+        try {
+            // First, create the DOM XMLSignatureFactory that will be used to
+            // generate the XMLSignature
+            XMLSignatureFactory fac = XMLSignatureFactory.getInstance("DOM");
+
+            // Next, create a Reference to a same-document URI that is an Object
+            // element and specify the SHA1 digest algorithm
+            String referenceId = "documentId";
+            Reference ref = fac.newReference("#" + referenceId, fac.newDigestMethod(DigestMethod.SHA256, null),
+                    Collections.singletonList(
+                            fac.newTransform(CanonicalizationMethod.INCLUSIVE, (TransformParameterSpec) null)),
+                    null, null);
+
+
+            // Next, create the referenced Object
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            Document doc = dbf.newDocumentBuilder().parse(new ByteArrayInputStream(xml.getBytes("UTF-8")));
+
+            DOMStructure domStructure = new DOMStructure(doc.getDocumentElement());
+            XMLObject theXmlInputObject = fac.newXMLObject(Collections.singletonList(domStructure), referenceId, null,
+                    null);
+
+            // Create the SignedInfo
+            SignedInfo si = fac.newSignedInfo(
+                    fac.newCanonicalizationMethod(CanonicalizationMethod.INCLUSIVE, (C14NMethodParameterSpec) null),
+                    fac.newSignatureMethod("http://www.w3.org/2001/04/xmldsig-more#rsa-sha256", null),
+//                    fac.newSignatureMethod(SignatureMethod.RSA_SHA1, null),
+                    Collections.singletonList(ref));
+
+            // Create the XMLSignature (but don't sign it yet)
+            XMLSignature signature = fac.newXMLSignature(si, getKeyInfo(privateKeyEntry),
+                    Collections.singletonList(theXmlInputObject), null, null);
+
+            // Create a DOMSignContext and specify the DSA PrivateKey for signing
+            // and the document location of the XMLSignature
+            DOMSignContext dsc = new DOMSignContext(privateKeyEntry.getPrivateKey(), doc);
+
+            // Lastly, generate the enveloping signature using the PrivateKey
+            signature.sign(dsc);
+
+            ByteArrayOutputStream returnBytes = new ByteArrayOutputStream();
+
+            TransformerFactory tf = TransformerFactory.newInstance();
+            Transformer trans = tf.newTransformer();
+            trans.transform(new DOMSource(doc), new StreamResult(returnBytes));
+
+            return returnBytes.toString("UTF-8");
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+
     public String sign(String xml, KeyStore.PrivateKeyEntry privateKeyEntry) {
+        return sign(xml, privateKeyEntry, Transform.ENVELOPED);
+    }
+
+    public String sign(String xml, KeyStore.PrivateKeyEntry privateKeyEntry, String transform) {
         try {
             // Create a DOM XMLSignatureFactory that will be used to
             // generate the enveloped signature.
@@ -37,26 +97,17 @@ public class XmlSigner {
             // Create a Reference to the enveloped document (in this case,
             // you are signing the whole document, so a URI of "" signifies
             // that, and also specify the SHA1 digest algorithm and
-            // the ENVELOPED Transform.
+            // the Transform method.
             Reference reference = fac.newReference("", fac.newDigestMethod(DigestMethod.SHA1, null),
-                    Collections.singletonList(fac.newTransform(Transform.ENVELOPED, (TransformParameterSpec) null)),
-                            null, null);
+                    Collections.singletonList(fac.newTransform(transform, (TransformParameterSpec) null)),
+                    null, null);
 
             // Create the SignedInfo.
             SignedInfo signedInfo = fac.newSignedInfo(fac.newCanonicalizationMethod(CanonicalizationMethod.INCLUSIVE,
-                    (C14NMethodParameterSpec) null), fac.newSignatureMethod(SignatureMethod.RSA_SHA1, null),
+                            (C14NMethodParameterSpec) null), fac.newSignatureMethod(SignatureMethod.RSA_SHA1, null),
                     Collections.singletonList(reference));
-
-            // Get the certificate from the private key.
-            X509Certificate cert = (X509Certificate) privateKeyEntry.getCertificate();
-
-            // Create the KeyInfo containing the X509Data.
-            KeyInfoFactory keyInfoFactory = fac.getKeyInfoFactory();
-            List x509Content = new ArrayList();
-            x509Content.add(cert.getSubjectX500Principal().getName());
-            x509Content.add(cert);
-            X509Data x509Data = keyInfoFactory.newX509Data(x509Content);
-            KeyInfo keyInfo = keyInfoFactory.newKeyInfo(Collections.singletonList(x509Data));
+            
+            KeyInfo keyInfo = getKeyInfo(privateKeyEntry);
 
             // Instantiate the document to be signed.
             DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
@@ -86,6 +137,20 @@ public class XmlSigner {
         }
     }
 
+    private KeyInfo getKeyInfo(KeyStore.PrivateKeyEntry privateKeyEntry) {
+        // Get the certificate from the private key.
+        X509Certificate cert = (X509Certificate) privateKeyEntry.getCertificate();
+
+        // Create the KeyInfo containing the X509Data.
+        KeyInfoFactory keyInfoFactory = KeyInfoFactory.getInstance();
+//        KeyInfoFactory keyInfoFactory = fac.getKeyInfoFactory();
+        List x509Content = new ArrayList();
+        x509Content.add(cert.getSubjectX500Principal().getName());
+        x509Content.add(cert);
+        X509Data x509Data = keyInfoFactory.newX509Data(x509Content);
+        return keyInfoFactory.newKeyInfo(Collections.singletonList(x509Data));
+    }
+
     public boolean verify(String signedXml, X509Certificate trustedCertificate) {
 
         try {
@@ -97,6 +162,8 @@ public class XmlSigner {
             DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
             dbf.setNamespaceAware(true);
             Document doc = dbf.newDocumentBuilder().parse(new ByteArrayInputStream(signedXml.getBytes("UTF-8")));
+
+            setIdAttributesAsId(doc);
 
             // Find Signature element.
             NodeList nodeList = doc.getElementsByTagNameNS(XMLSignature.XMLNS, "Signature");
@@ -120,6 +187,14 @@ public class XmlSigner {
             return coreValidity;
         } catch (Exception e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    private void setIdAttributesAsId(Document doc) {
+        NodeList elementsByTagName = doc.getElementsByTagName("bankIdSignedData");
+        if (elementsByTagName.getLength() > 0) {
+            Element bankIdSignedDataElement = (Element) elementsByTagName.item(0);
+            bankIdSignedDataElement.setIdAttribute("Id", true);
         }
     }
 
